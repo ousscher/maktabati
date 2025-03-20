9f52fff789e7594ee6436bfcf222ce342df521c8
ousscher <lo_cherguelaine@esi.dz>
2025-03-20 16:18:34 +0100

    Trying to add some dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,25 @@
 import i18n from "./i18n.config.mjs";
 
 const nextConfig = {
-  reactStrictMode: true,
-  i18n
+  i18n: i18n,
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin'],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      '@google-cloud/firestore': 'commonjs @google-cloud/firestore'
+    });
+    
+    // Ajoutez cette partie si vous avez besoin de polyfills
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false
+    };
+
+    return config;
+  }
 };
 
 export default nextConfig;
